54d9cefe14f7c7493509cc88365578452ed09177
Henelyn <henelyn@gmail.com>
2022-03-20 14:12:38 +0200

    [fix] - constants string

--- frontend/src/app/constants/constant.ts
+++ frontend/src/app/constants/constant.ts
@@ -1,4 +1,2 @@
-import {config} from "rxjs";
-
-export const BACKEND_CONFIG_ROOT = "http://localhost:8080/config" //for the backend address
-export const BACKEND_CONFIG_URL = '${BACKEND_CONFIG_ROOT}`/config' //for the backend address
+export const BACKEND_CONFIG_ROOT = "http://localhost:8080" //for the backend address
+export const BACKEND_CONFIG_URL = `${BACKEND_CONFIG_ROOT}/config` //for the backend address
